0786100bfe45ece7dcb1c23b94996322d0bf2a0e
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-02 01:42:12 +0000

    created index.js file and created an app.listen device with process.env.port

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(process.env.port || 4000, function(){
+	console.log("now listening for requests");
+});
+
+// const express = require('express');
+
+// // set up express app
+// const app = express();
+
+// // listen for requests
+// app.listen(process.env.port || 4000, function(){
+//    console.log('now listening for requests');
+// });
